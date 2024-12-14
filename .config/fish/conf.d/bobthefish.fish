5dae8ad7fe428bc3f60fcae898d6cec35628e84e
horrifyingHorse <excuseplanation@gmail.com>
2024-12-15 03:59:56 +0530

    config init commit

---
+++ .config/fish/conf.d/bobthefish.fish
@@ -0,0 +1,19 @@
+# set -g theme_color_scheme terminal2-dark-white
+set -g theme_color_scheme nord
+set -g theme_display_user ssh
+# set -g default_user bobthecow
+
+set -g fish_prompt_pwd_dir_length 0
+set -g theme_show_project_parent no
+
+# set -g theme_display_vi yes
+
+# set -g default_user your_normal_user
+
+# set -g theme_newline_cursor yes
+
+set -g theme_newline_prompt "\$ "
+
+set -g theme_display_screen no
+
+set -g theme_display_date no
---
+++ readme.md
@@ -0,0 +1,5 @@
+# `.datfiles`
+
+Font:
+[Commit Mono Nerd Font](
+https://github.com/ryanoasis/nerd-fonts/releases/download/v3.3.0/CommitMono.zip)
