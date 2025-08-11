2c3f071dce158dab265e59232c1b21f2755785e3
horrifyingHorse <excuseplanation@gmail.com>
2025-08-11 21:43:16 +0530

    more firenvim config

--- .config/nvim/lua/configs/firenvim.lua
+++ .config/nvim/lua/configs/firenvim.lua
@@ -3,3 +3,14 @@ vim.g.firenvim_config = {
     ['https://discord.com/.*'] = { takeover = 'never', priority = 1 },
   },
 }
+
+if vim.g.started_by_firenvim then
+  vim.api.nvim_create_autocmd('BufEnter', {
+    callback = function()
+      local url = vim.api.nvim_buf_get_name(0)
+      if url:match 'takeuforward.org' or url:match 'leetcode.com' then
+        vim.bo.filetype = 'cpp'
+      end
+    end,
+  })
+end
