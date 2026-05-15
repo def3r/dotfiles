9d82e99bdeb206cade48e580dd36991da7d78095
Ayaan Khan <excuseplanation@gmail.com>
2026-05-15 23:36:32 +0530

    more hyprland retar-icemaxxing

--- symIt.sh
+++ symIt.sh
@@ -16,11 +16,13 @@ ln -sf $dotfiles/.config/fish $dest/.config/fish
 ln -sf $dotfiles/.config/gh   $dest/.config/gh
 ln -sf $dotfiles/.config/nvim $dest/.config/nvim
 ln -sf $dotfiles/.config/omf  $dest/.config/omf
+ln -sf $dotfiles/.config/waybar  $dest/.config/waybar
 
-ln -sf $dotfiles/.config/hypr/scripts $dest/.config/hypr/scripts
+ln -sf $dotfiles/.config/hypr/scripts       $dest/.config/hypr/scripts
 ln -sf $dotfiles/.config/hypr/hyprland.conf $dest/.config/hypr/hyprland.conf
 
-ln -sf "$dotfiles/.bashrc"    "$dest/.bashrc"
-ln -sf "$dotfiles/.tmux.conf" "$dest/.tmux.conf"
-ln -sf "$dotfiles/.vimrc"     "$dest/.vimrc"
-ln -sf "$dotfiles/.zshrc"     "$dest/.zshrc"
+ln -sf "$dotfiles/.bashrc"          "$dest/.bashrc"
+ln -sf "$dotfiles/.tmux.conf"       "$dest/.tmux.conf"
+ln -sf "$dotfiles/.vimrc"           "$dest/.vimrc"
+ln -sf "$dotfiles/.zshrc"           "$dest/.zshrc"
+ln -sf "$dotfiles/kitty/kitty.conf" "$dest/.config/kitty/kitty.conf"
